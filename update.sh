fa9bc3ea3787ffd945cc1abbb0f05a8e98a89c11
Mark Taylor <mark.taylor@sd71.bc.ca>
2023-06-12 16:03:08 -0700

    update to run as admin

--- update.sh
+++ update.sh
@@ -9,33 +9,48 @@
 
 #!/bin/bash
 
-# Set the installation directory in the user profile
-INSTALL_DIR="$HOME/bin"
-
-# Create the installation directory if it doesn't exist
-mkdir -p "$INSTALL_DIR"
-
 # Check if Winget is already installed
 if ! command -v winget &> /dev/null; then
     echo "Winget is not installed. Installing Winget..."
-    # Download and extract the Winget package
-    curl -o winget.zip -L https://github.com/microsoft/winget-cli/releases/latest/download/winget-cli.zip
-    unzip -o winget.zip -d "$INSTALL_DIR"
-    rm winget.zip
+    # Download and install the Winget installer
+    curl -o winget_installer.msi -L https://github.com/microsoft/winget-cli/releases/latest/download/winget-cli-x64.msi
+    msiexec /i winget_installer.msi /quiet
+    rm winget_installer.msi
     echo "Winget installed successfully!"
 else
     echo "Winget is already installed."
 fi
 
-# Set up the PATH for Winget
-WINGET_BIN_DIR="$INSTALL_DIR/winget"
-echo 'export PATH="$PATH:'"$WINGET_BIN_DIR"'"' >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
+# Check if Git is already installed
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Installing Git..."
+    # Download and install the Git installer
+    curl -o git_installer.exe -L https://github.com/git-for-windows/git/releases/latest/download/Git-2.33.1-64-bit.exe
+    ./git_installer.exe /VERYSILENT /NORESTART
+    rm git_installer.exe
+    echo "Git installed successfully!"
+else
+    echo "Git is already installed."
+fi
 
-# Install or update Visual Studio Code using Winget
+# Get the system root directory
+SYSTEM_ROOT=$(cygpath -w /)
+
+# Add Winget to the system environment variables and PATH
+echo "Adding Winget to system environment variables and PATH..."
+echo 'setx /M PATH "%PATH%;%SYSTEM_ROOT%\\Program Files\\Microsoft\\WinGet"' | cmd
+echo "Winget added successfully!"
+
+# Add Git to the system environment variables and PATH
+echo "Adding Git to system environment variables and PATH..."
+echo 'setx /M PATH "%PATH%;%SYSTEM_ROOT%\\Program Files\\Git\\cmd"' | cmd
+echo "Git added successfully!"
+
+# Update Visual Studio Code using Winget
 echo "Updating Visual Studio Code..."
-winget install --exact --id Microsoft.VisualStudioCode
+winget upgrade --name "Microsoft.VisualStudioCode"
 echo "Update completed!"
+
 
 
 #rerun the command: bash update.sh if needed
