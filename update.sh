9ec2a55dc8707958f032a18fec36f28937e8cdeb
Mark Taylor <mark.taylor@sd71.bc.ca>
2023-06-12 15:47:44 -0700

    install to profile

--- update.sh
+++ update.sh
@@ -7,14 +7,21 @@
 #echo "Updating Visual Studio Code..."
 #code --install-extension ms-vscode.vscode-typescript-tslint-plugin --force # Example extension, replace with your desired extensions
 
+#!/bin/bash
+
+# Set the installation directory in the user profile
+INSTALL_DIR="$HOME/bin"
+
+# Create the installation directory if it doesn't exist
+mkdir -p "$INSTALL_DIR"
 
 # Check if Winget is already installed
 if ! command -v winget &> /dev/null; then
     echo "Winget is not installed. Installing Winget..."
-    # Download and install the Winget installer
-    curl -o winget_installer.msi -L https://github.com/microsoft/winget-cli/releases/latest/download/winget-cli-x64.msi
-    msiexec /i winget_installer.msi /quiet
-    rm winget_installer.msi
+    # Download and extract the Winget package
+    curl -o winget.zip -L https://github.com/microsoft/winget-cli/releases/latest/download/winget-cli.zip
+    unzip -o winget.zip -d "$INSTALL_DIR"
+    rm winget.zip
     echo "Winget installed successfully!"
 else
     echo "Winget is already installed."
@@ -23,10 +30,10 @@ fi
 # Check if Git is already installed
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Installing Git..."
-    # Download and install the Git installer
-    curl -o git_installer.exe -L https://github.com/git-for-windows/git/releases/latest/download/Git-2.33.1-64-bit.exe
-    ./git_installer.exe /SP /VERYSILENT /NORESTART
-    rm git_installer.exe
+    # Download and extract the Git package
+    curl -o git.zip -L https://github.com/git-for-windows/git/releases/latest/download/PortableGit-2.33.1-64-bit.7z.exe
+    7z x -y -bd git.zip -o"$INSTALL_DIR"
+    rm git.zip
     echo "Git installed successfully!"
 else
     echo "Git is already installed."
@@ -34,7 +41,7 @@ fi
 
 # Add Winget and Git to the user's environment variables
 echo "Adding Winget and Git to user environment variables..."
-echo 'export PATH="$PATH:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files/Microsoft/Winget"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/bin"' >> "$HOME/.bashrc"
 echo "Environment variables updated!"
 
 # Update Visual Studio Code using Winget
