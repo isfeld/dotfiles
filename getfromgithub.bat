13372238678c527dfa9fba12dc399b89d530f638
Mark Taylor <mark.taylor@sd71.bc.ca>
2023-06-15 17:13:16 -0700

    v4

--- getfromgithub.bat
+++ getfromgithub.bat
@@ -1,6 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
  
-New-Item "C:\Install\New" -Type Directory
+powershell New-Item "C:\Install\New" -Type Directory
 
 cd New
 
